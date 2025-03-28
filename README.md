932a2090c2a3272540db27329a66ccebd63094b4
selena <selenaho2000@gmail.com>
2025-03-27 23:36:14 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# Web Development Project 4 - *Cats*
+
+Submitted by: **Selena Ho**
+
+This web app: **makes calls to thecatapi to display images of cats and breed details**
+
+Time spent: **X** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [X] **Clicking a button creates a new API fetch request and displays at least three attributes from the returned JSON data**
+- [X] **Only one item/API call is viewable at a time**
+- [X] **API calls appear random to the user**
+- [X] **At least one image is displayed per API call**
+- [X] **Clicking on a displayed value for one attribute adds it to a displayed ban list**
+  - [ ] To ensure an accurate grade, your recording **must** show that when clicked, an attribute in the ban list is immediately removed from the list of banned attributes
+- [ ] **Attributes on the ban list prevent further images/API results with that attribute from being displayed**
+
+The following **optional** features are implemented:
+
+- [ ] Multiple types of attributes can be added to the ban list
+- [ ] Users can see a stored history of their previously viewed items from their session
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://i.imgur.com/link/to/your/gif/file.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with ScreenToGif  
+<!-- Recommended tools:
+[Kap](https://getkap.co/) for macOS
+[ScreenToGif](https://www.screentogif.com/) for Windows
+[peek](https://github.com/phw/peek) for Linux. -->
+
+## Notes
+
+Describe any challenges encountered while building the app.
+
+## License
+
+    Copyright 2025 Selena Ho
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import { use, useState } from 'react'
+import './App.css'
+
+function App() {
+      
+    const makeQuery = () => {
+        let key = "live_0GcP0uXfE7BXn3nparH6XxfShQ22ZHbdCodhkKQV8Y7BBsmHE8cRPjd0WnKUxVkT";
+        let query = `https://api.thecatapi.com/v1/images/search?limit=10&size=med&has_breeds=true&order=RANDOM&api_key=${key}`;
+        callAPI(query).catch(console.error);
+    }
+
+    const [currentImage, setCurrentImage] = useState(null);
+    const [currentName, setCurrentName] = useState("");
+    const [currentTemperament, setCurrentTemperament] = useState("");
+    const [currentDescription, setCurrentDescription] = useState("");
+
+    const [banList, setBanList] = useState([]);
+    const updateBanList = (name) => {
+        if(banList.includes(name)) {
+            //name is already banned so unban it
+            setBanList(banList.filter(item => item !== name));
+        }
+        else {
+            //name isn't banned so ban it
+            setBanList([...banList, name]);
+        }
+    }
+
+    const callAPI = async (query) => {
+        const response = await fetch(query);
+        const json = await response.json();
+        if (json == null) {
+            alert("Oops! Something went wrong with that query, let's try again!")
+        }
+        else {
+            let currIndex = 0;
+            while (currIndex < json.length && banList.includes(json[currIndex].breeds[0].name)) {
+                currIndex++;
+            }
+            if (currIndex < json.length) {
+                setCurrentName(json[currIndex].breeds[0].name);
+                setCurrentImage(json[currIndex].url);
+                setCurrentDescription(json[currIndex].breeds[0].description);
+                setCurrentTemperament(json[currIndex].breeds[0].temperament);
+            } else {
+                alert("No available breeds found that are not in the ban list.");
+            }
+            //console.log(currentImage);
+            //reset();
+        }
+        
+        console.log(json);
+    }
+
+    return (
+        <>
+            <h1>Cats!</h1>
+            <p>made using thecatapi</p>
+            <h3>Currently Banned:</h3>
+            {banList.map((ban) => (
+                <button type='submit' onClick={() => updateBanList(ban)}>{ban}</button>
+            ))}
+            <br></br>
+            {currentImage ? (
+                <img
+                src={currentImage}
+                alt="Image returned"
+                />
+            ) : (
+                <div> </div>
+            )}
+            <br></br>
+            {currentName ? (
+                <button type='submit' onClick={()=> updateBanList(currentName)}>{currentName}</button>
+            ) : (
+                <div> </div>
+            )}
+            <br></br>
+            {currentTemperament ? (
+                <h5>{currentTemperament}</h5>
+            ) : (
+                <div> </div>
+            )}
+            {currentDescription ? (
+                <p>{currentDescription}</p>
+            ) : (
+                <div> </div>
+            )}
+            <button type='submit' onClick={makeQuery}>Next</button>
+        </>
+    )
+}
+
+export default App
